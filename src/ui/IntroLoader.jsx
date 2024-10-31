64dd856b3f41d76194b95550174686c5cacf9f2f
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-10-31 08:26:24 +0200

    clean

--- src/ui/IntroLoader.jsx
+++ src/ui/IntroLoader.jsx
@@ -1,10 +1,7 @@
-import { useRef } from "react";
 import intro from "../assets/lotties/intro.json";
 import Lottie from "react-lottie";
 
 export default function IntroLoader({ className }) {
-  const lottieRef = useRef(null);
-
   const defaultOptions = {
     loop: false,
     autoplay: true,
@@ -16,7 +13,7 @@ export default function IntroLoader({ className }) {
 
   return (
     <div className={`intro_loader ${className}`}>
-      <Lottie options={defaultOptions} ref={lottieRef} />
+      <Lottie options={defaultOptions} />
     </div>
   );
 }
