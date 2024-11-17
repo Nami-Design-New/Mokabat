c41ae9e93e010f23dbc31cbb1e822a4aa99413e6
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-11-17 15:08:29 +0200

    projects page

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,92 +1,66 @@
+import { useTranslation } from "react-i18next";
+import useGetProjects from "../hooks/projects/useGetProjects";
+
 export default function Projects() {
+  const { t } = useTranslation();
+  const { data: projects } = useGetProjects();
+
   return (
     <section className="projects_page">
       <div className="container">
         <h1 data-aos="fade-up">
-          Mokabat Expertise In <span>Driving Innovation</span>
+          {t("mokabatExperience")} <span>{t("drivingInnovation")}</span>
         </h1>
         <div className="projects">
-          <div className="row">
-            <div className="col-lg-6 col-12 p-2">
-              <div className="project_card">
-                <h3 className="title" data-aos="fade-up">
-                  MSME PLATFORM
-                </h3>
-                <p className="subtitle" data-aos="fade-up">
-                  A one-stop solution to simplify banking & day to day business
-                  need.
-                </p>
-
-                <p className="description" data-aos="fade-up">
-                  A Digital platform integrated with the Ecosystem to serve
-                  SME’s to manage their financial affairs & Growing their
-                  business
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-6 col-12 p-2">
-              <div className="img" data-aos="zoom-in-up">
-                <img src="/images/MSME-Platform.png" alt="" />
-              </div>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-lg-6 col-12 p-2 order-lg-0 order-1">
-              <div className="img" data-aos="zoom-in-up">
-                <img src="/images/Home-Buying-Ecosystem.webp" alt="" />
-              </div>
-            </div>
-            <div className="col-lg-6 col-12 p-2 order-lg-1 order-0 ">
-              <div className="project_card">
-                <h3 className="title" data-aos="fade-up">
-                  HOME BUYING ECOSYSTEM
-                </h3>
-                <p className="subtitle" data-aos="fade-up">
-                  A Fully Digitalized, Easy To Use Ecosystem Supporting
-                  Customers Across Their Home Buying Journey & Brokers Managing
-                  their Business
-                </p>
-
-                <p className="description" data-aos="fade-up">
-                  Initiate Application Through Integrated Broker Partner Search
-                  For Property On Partner portals Get Additional Post-Sales
-                  Support Such As Furnishing, Designing… Integrated Eligibility
-                  Check With All Relevant Entities Plan Re-Payment And Request
-                  For House Maintenance Support Apply And Receive Instant
-                  In-Principle Approval
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-lg-6 col-12 p-2">
-              <div className="project_card">
-                <h3 className="title" data-aos="fade-up">
-                  CORPORATE RM WORKBENCH
-                </h3>
-                <p className="subtitle" data-aos="fade-up">
-                  An AI-Powered Frontend Enabling A Step change In Client
-                  Relationships. Corporate RMs (Relationship Managers)
-                </p>
-
-                <p className="description" data-aos="fade-up">
-                  Detailed RM Performance Management AI-Driven Identification Of
-                  Client Potential Automated Pricing Review & Upselling Insights
-                  Collaboration & Lead Sharing With Product Specialists AI
-                  Recommendations For New Opportunities Bottom Up Target Setting
-                  Based On Potential Instant Pipeline Reporting & Account
-                  Planning
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-6 col-12 p-2">
-              <div className="img" data-aos="zoom-in-up">
-                <img src="/images/CORPORATE-RM.webp" alt="" />
-              </div>
-            </div>
-          </div>
+          {projects?.map((project, index) => {
+            if (index % 2 !== 0) {
+              return (
+                <div className="row" key={project.id}>
+                  <div className="col-lg-6 col-12 p-2 order-lg-0 order-1">
+                    <div className="img" data-aos="zoom-in-up">
+                      <img src={project.image} alt={project.title} />
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-12 p-2 order-lg-1 order-0">
+                    <div className="project_card">
+                      <h3 className="title" data-aos="fade-up">
+                        {project.title}
+                      </h3>
+                      <p className="subtitle" data-aos="fade-up">
+                        {project.sub_title}
+                      </p>
+                      <p className="description" data-aos="fade-up">
+                        {project.description}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              );
+            } else {
+              return (
+                <div className="row" key={project.id}>
+                  <div className="col-lg-6 col-12 p-2">
+                    <div className="project_card">
+                      <h3 className="title" data-aos="fade-up">
+                        {project.title}
+                      </h3>
+                      <p className="subtitle" data-aos="fade-up">
+                        {project.sub_title}
+                      </p>
+                      <p className="description" data-aos="fade-up">
+                        {project.description}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-12 p-2">
+                    <div className="img" data-aos="zoom-in-up">
+                      <img src={project.image} alt={project.title} />
+                    </div>
+                  </div>
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     </section>
